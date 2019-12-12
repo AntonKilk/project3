ca0e499424824c8466d3d08ba0ed21e1338fb517
AntonKilk <ant.kilk@gmail.com>
2019-12-12 17:03:14 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+let Height = (props) => {
+  
+  return <div>
+    <div>{props.title}: {props.min}
+    <input type="range"
+    min={props.min}
+    max={props.max}
+    value={props.value}
+    onChange={props.setHeight}
+    ></input>{props.max}
+    <b> {props.value} {props.unit}</b>
     </div>
-  );
+  </div>
 }
 
-export default App;
+let Weight = (props) => {
+  
+  return <div>
+    <div>{props.title}: {props.min}
+    <input type="range"
+    min={props.min}
+    max={props.max}
+    value={props.value}
+    onChange={props.setWeight}
+    ></input>{props.max}
+    <b> {props.value} {props.unit}</b>
+    </div>
+  </div>
+}
+
+
+
+export default class App extends React.Component {
+  state = {
+    height: "150",
+    weight: "50"
+  }
+
+  setHeight = (e) => {
+    this.setState({height: e.target.value})
+  }
+
+  setWeight = (e) => {
+    this.setState({weight: e.target.value})
+  }
+
+  bmi = () => {
+    let hNum = parseInt(this.state.height)
+    let wNum = parseInt(this.state.weight) 
+    let bmi = ( wNum /(hNum/100) ** 2 ).toFixed(1)
+    return bmi
+  }
+
+  render() {
+    let diagnose = this.bmi() <= 18.4 ? "Underweight" :
+    this.bmi() <= 25 ? "Normal Weight" : 
+    this.bmi() <= 30 ? "Overweight" :
+    this.bmi() > 30 ? "Obese" : ""
+
+    return <div>
+      <Height
+        min={90}
+        max={245}
+        title={"Height"}
+        unit={"cm"}
+        value={this.state.height} 
+        setHeight={this.setHeight}
+      />
+      <Weight
+        min={35}
+        max={200}
+        title={"Weight"}
+        unit={"kg"}
+        value={this.state.weight} 
+        setWeight={this.setWeight}
+      />
+      <div>BMI: <b> {this.bmi()}</b> <b> {diagnose}</b></div>
+    </div>
+  }
+  
+}
+
+
